fdbb6ed85ea1ff42a374094046e21cee50768f51
赵鑫 <645722459@qq.com>
2016-11-14 20:48:49 +0800

    完成

---
+++ app/src/main/java/com/example/dllo/bibilala/entity/find/TopicEntity.java
@@ -0,0 +1,89 @@
+package com.example.dllo.bibilala.entity.find;
+ /*
+        quu..__
+         $$$b  `---.__
+          "$$b        `--.                          ___.---uuudP
+           `$$b           `.__.------.__     __.---'      $$$$"              .
+             "$b          -'            `-.-'            $$$"              .'|
+               ".                                       d$"             _.'  |
+                 `.   /                              ..."             .'     |
+                   `./                           ..::-'            _.'       |
+                    /                         .:::-'            .-'         .'
+                   :                          ::''\          _.'            |
+                  .' .-.             .-.           `.      .'               |
+                  : /'$$|           .@"$\           `.   .'              _.-'
+                 .'|$u$$|          |$$,$$|           |  <            _.-'
+                 | `:$$:'          :$$$$$:           `.  `.       .-'
+                 :                  `"--'             |    `-.     \
+                :                |                |#'     `..'`..'          `                 \                                   xXX|     /    ./
+                  \                                xXXX'|    /   ./
+                  /`-.                                  `.  /   /
+                 :    `-  ...........,                   | /  .'
+                 |         ``:::::::'       .            |<    `.
+                 |             ```          |           x| \ `.:``.
+                 |                         .'    /'   xXX|  `:`M`M':.
+                 |    |                    ;    /:' xXXX'|  -'MMMMM:'
+                 `.  .'                   :    /:'       |-'MMMM.-'
+                  |  |                   .'   /'        .'MMM.-'
+                  `'`'                   :  ,'          |MMM<
+                    |                     `'            |tbap\
+                     \                                  :MM.-'
+                      \                 |              .''
+                       \.               `.            /
+                        /     .:::::::.. :           /
+                       |     .:::::::::::`.         /
+                       |   .:::------------\       /
+                      /   .''               >::'  /
+                      `',:                 :    .'
+                                           `:.:'
+
+         
+        */
+
+import java.util.List;
+
+/**
+ * Created by dllo on 16/11/14.
+ */
+
+public class TopicEntity {
+    private int code;
+    private int total;
+    private int pages;
+
+    private List<ListEntity> list;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public List<ListEntity> getList() {
+        return list;
+    }
+
+    public void setList(List<ListEntity> list) {
+        this.list = list;
+    }
+
+
+}
